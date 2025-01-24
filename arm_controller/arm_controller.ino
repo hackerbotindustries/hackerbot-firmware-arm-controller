915ebaafbb851e6adacbe846f78ae283f267690d
Ian <ian@arobodude.com>
2025-01-23 18:01:53 -0700

    Added ANGLE and ANGLES commands

--- arm_controller/arm_controller.ino
+++ arm_controller/arm_controller.ino
@@ -10,11 +10,18 @@ controller to the myCobot 280 arm and Hackerbot gripper.
 
 #include <Dynamixel2Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include <MyCobotBasic.h>
 #include <SerialCmd.h>
 #include <Wire.h>
 
+// Arm Controller software version
+#define VERSION_NUMBER 1
+
+// I2C address (0x5C)
+#define I2C_ADDRESS    92
+
 // Set up variables and constants for dynamixel control
-#define DXL_SERIAL   Serial1
+#define DXL_SERIAL     Serial1
 
 const int DXL_DIR_PIN = 2;
 const uint8_t DXL_GRIPPER_ID = 1;
@@ -23,59 +30,96 @@ const float DXL_PROTOCOL_VERSION = 2.0;
 Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
 using namespace ControlTableItem;
 
-unsigned long startMillis;
-unsigned long currentMillis;
-byte RxArray[32];
-
 const float CALIBRATION_CURRENT = 60.0;
 const float CALIBRATION_OFFSET = 650.0;
 const double CALIBRATION_MIN = -1000;
 const double CALIBRATION_MAX = 100000;
 double homeCalibration;
 
+// Timing variables
+unsigned long startMillis;
+unsigned long currentMillis;
+
 // Set up the onboard neopixel
 Adafruit_NeoPixel onboard_pixel(1, PIN_NEOPIXEL);
 
-// Set up the serial command processor
-#define SERIALCMD_MAXCMDNUM 20    // Max number of commands
-#define SERIALCMD_MAXCMDLNG 20    // Max command name length
-#define SERIALCMD_MAXBUFFER 32    // Max buffer length
+// Other defines and variables
+byte I2CRxArray[16];
+byte I2CTxArray[16];
+byte cmd = 0;
 
+// Set up the myCobot280
+Uart Serial2 (&sercom2, 9, 10, SERCOM_RX_PAD_1, UART_TX_PAD_2);
+MyCobotBasic myCobot;
+
+// Set up the serial command processor
 SerialCmd mySerCmd(Serial);
+int8_t ret;
 
 
 // -------------------------------------------------------
 // I2C Rx Handler
 // -------------------------------------------------------
 void I2C_RxHandler(int numBytes) {
-  Serial.print("[INFO] I2C Byte Received... ");
+  String query = String();
+  char CharArray[32];
+
+  Serial.print("INFO: I2C Byte Received... ");
   for (int i = 0; i < numBytes; i++) {
-    RxArray[i] = Wire.read();
+    I2CRxArray[i] = Wire.read();
     Serial.print("0x");
-    Serial.print(RxArray[i], HEX);
+    Serial.print(I2CRxArray[i], HEX);
     Serial.print(" ");
   }
 
   Serial.println();
 
   // Parse incoming commands
-  switch (RxArray[0]) {
-    case 0x01: // Set_IDLE Command - Params(0 = Off, 1 = On)
-      Serial.println("Set_IDLE command received");
-      if (RxArray[1] == 0x00) {
-        //idle = 0;
-      } else {
-        //idle = 1;
-      }
+  switch (I2CRxArray[0]) {
+    case 0x01: // Ping
+      cmd = 0x01;
+      I2CTxArray[0] = 0x01;
       break;
-    case 0x02: // Set_Direction Command - Params(rotation h, rotation l, pitch h, pitch l, speed)
-      Serial.println("Set_Direction command received");
-      //position_turn = ((RxArray[1] << 8) + RxArray[2]) * 0.1;
-      //position_speed = RxArray[5];
-      //newMovement = 1;
+    case 0x02: // Version
+      cmd = 0x02;
+      I2CTxArray[0] = VERSION_NUMBER;
+      break;
+    case 0x10: // Set_ANGLE Command - Params(joint, angle h, angle l, speed)
+      Serial.println("INFO: Set_ANGLE command received");
+
+      query = "ANGLE," + (String)(I2CRxArray[1]) + "," + (String)(((I2CRxArray[2] << 8) + I2CRxArray[3]) * 0.1) + "," + (String)(I2CRxArray[4]);
+      
+      // Convert the query string to a char array
+      query.toCharArray(CharArray, query.length() + 1);
+      Serial.println(CharArray);
+      ret = mySerCmd.ReadString(CharArray);
       break;
   }
 }
+
+
+// -------------------------------------------------------
+// I2C Tx Handler
+// -------------------------------------------------------
+void I2C_TxHandler(void) {
+  switch (cmd) {
+    case 0x01: // Ping
+      Wire.write(I2CTxArray[0]);
+      break;
+    case 0x02: // Version
+      Wire.write(I2CTxArray[0]);
+      break;
+  }
+}
+
+
+// -------------------------------------------------------
+// Serial2 SERCOM Handler
+// -------------------------------------------------------
+void SERCOM2_Handler() {
+  Serial2.IrqHandler();
+}
+
 
 // -------------------------------------------------------
 // User Functions
@@ -92,7 +136,8 @@ void send_PING(void) {
   sendOK();
 }
 
-void set_LEDON(void) {
+// OPEN
+void set_OPEN(void) {
   double current_reading = 0.0;
 
   dxl.setGoalPosition(DXL_GRIPPER_ID, homeCalibration - 10000, UNIT_DEGREE);
@@ -118,7 +163,8 @@ void set_LEDON(void) {
   sendOK();
 }
 
-void set_LEDOFF(void) {
+// CLOSE
+void set_CLOSE(void) {
   double current_reading = 0.0;
 
   dxl.setGoalPosition(DXL_GRIPPER_ID, homeCalibration, UNIT_DEGREE);
@@ -144,6 +190,122 @@ void set_LEDOFF(void) {
   sendOK();
 }
 
+// ANGLE
+void set_ANGLE(void) {
+  float jointParam = atof(mySerCmd.ReadNext());
+  float angleParam = atof(mySerCmd.ReadNext());
+  float speedParam = atof(mySerCmd.ReadNext());
+
+  if (speedParam == NULL) {
+    mySerCmd.Print((char *) "ERROR: Missing parameter\r\n");
+    return;
+  }
+
+  if (jointParam < 0) {
+    jointParam = 0;
+  } else if (jointParam > 6) {
+    jointParam = 6;
+  }
+
+  if (angleParam < -165.0) {
+    angleParam = -165.0;
+  } else if (angleParam > 165.0) {
+    angleParam = 165.0;
+  }
+
+  if (speedParam < 0) {
+    speedParam = 0;
+  } else if (speedParam > 100) {
+    speedParam = 100;
+  }
+
+  mySerCmd.Print((char *) "STATUS: Setting the angle of joint ");
+  mySerCmd.Print((int)jointParam);
+  mySerCmd.Print((char *) " to ");
+  mySerCmd.Print(angleParam);
+  mySerCmd.Print((char *) " degrees at speed ");
+  mySerCmd.Print((int)speedParam);
+  mySerCmd.Print((char *) "\r\n");
+
+  myCobot.writeAngle((int)jointParam, angleParam, (int)speedParam);
+
+  sendOK();
+}
+
+// ANGLES
+void set_ANGLES(void) {
+  float joint1Param = atof(mySerCmd.ReadNext());
+  float joint2Param = atof(mySerCmd.ReadNext());
+  float joint3Param = atof(mySerCmd.ReadNext());
+  float joint4Param = atof(mySerCmd.ReadNext());
+  float joint5Param = atof(mySerCmd.ReadNext());
+  float joint6Param = atof(mySerCmd.ReadNext());
+  float speedParam = atof(mySerCmd.ReadNext());
+
+  if (speedParam == NULL) {
+    mySerCmd.Print((char *) "ERROR: Missing parameter\r\n");
+    return;
+  }
+
+  if (joint1Param < -165.0)
+    joint1Param = 165.0;
+  else if (joint1Param > 165.0)
+    joint1Param = 165.0;
+
+  if (joint2Param < -165.0)
+    joint2Param = 165.0;
+  else if (joint2Param > 165.0)
+    joint2Param = 165.0;
+
+  if (joint3Param < -165.0)
+    joint3Param = 165.0;
+  else if (joint3Param > 165.0)
+    joint3Param = 165.0;
+
+  if (joint4Param < -165.0)
+    joint4Param = 165.0;
+  else if (joint4Param > 165.0)
+    joint4Param = 165.0;
+
+  if (joint5Param < -165.0)
+    joint5Param = 165.0;
+  else if (joint5Param > 165.0)
+    joint5Param = 165.0;
+
+  if (joint6Param < -175.0)
+    joint6Param = 175.0;
+  else if (joint6Param > 175.0)
+    joint6Param = 175.0;
+
+  if (speedParam < 0)
+    speedParam = 0;
+  else if (speedParam > 100)
+    speedParam = 100;
+
+  Angles angles = {joint1Param, joint2Param, joint3Param, joint4Param, joint5Param, joint6Param};
+
+  mySerCmd.Print((char *) "STATUS: Setting the angle of the joints to (1) ");
+  mySerCmd.Print(joint1Param);
+  mySerCmd.Print((char *) ", (2) ");
+  mySerCmd.Print(joint2Param);
+  mySerCmd.Print((char *) ", (3) ");
+  mySerCmd.Print(joint3Param);
+  mySerCmd.Print((char *) ", (4) ");
+  mySerCmd.Print(joint4Param);
+  mySerCmd.Print((char *) ", (5) ");
+  mySerCmd.Print(joint5Param);
+  mySerCmd.Print((char *) ", (6) ");
+  mySerCmd.Print(joint6Param);
+  mySerCmd.Print((char *) " degrees at speed ");
+  mySerCmd.Print((int)speedParam);
+  mySerCmd.Print((char *) "\r\n");
+
+  myCobot.writeAngles(angles, speedParam);
+
+  sendOK();
+}
+
+// CAL
 void run_CALIBRATION(void) {
   double current_reading = 0.0;
 
@@ -188,23 +350,36 @@ void setup() {
 
   // Define serial commands
   mySerCmd.AddCmd("PING", SERIALCMD_FROMALL, send_PING);
-  mySerCmd.AddCmd("LEDON", SERIALCMD_FROMALL, set_LEDON);
-  mySerCmd.AddCmd("LEDOFF", SERIALCMD_FROMALL, set_LEDOFF);
   mySerCmd.AddCmd("CAL", SERIALCMD_FROMALL, run_CALIBRATION);
+  mySerCmd.AddCmd("OPEN", SERIALCMD_FROMALL, set_OPEN);
+  mySerCmd.AddCmd("CLOSE", SERIALCMD_FROMALL, set_CLOSE);
+  mySerCmd.AddCmd("ANGLE", SERIALCMD_FROMALL, set_ANGLE);
+  mySerCmd.AddCmd("ANGLES", SERIALCMD_FROMALL, set_ANGLES);
 
   // Set up the dynamixel serial port
   dxl.begin(57600);
   dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
 
-  // Set up the I2C port
-  Wire.begin(91); // Initialize I2C (Slave Mode: address=0x5B)
+  // Initialize I2C (Slave Mode: address=0x5C)
+  Wire.begin(I2C_ADDRESS);
   Wire.onReceive(I2C_RxHandler);
+  Wire.onRequest(I2C_TxHandler);
 
   // Configure the gripper dynamixel
   dxl.ping(DXL_GRIPPER_ID);
   
   dxl.writeControlTableItem(PROFILE_ACCELERATION, DXL_GRIPPER_ID, 10);
   dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_GRIPPER_ID, 40);
+
+  // Finish Serial2 setup and configure the myCobot280
+  PortGroup *port = digitalPinToPort(9);
+  port->OUTSET.reg = g_APinDescription[9].ulPin ;
+  port->PINCFG[g_APinDescription[9].ulPin].reg |= PORT_PINCFG_PULLEN;
+
+  myCobot.setup(&Serial2);
+  delay(100);
+  myCobot.powerOn();
+  delay(100);
 
   // Configure the onboard neopixel
   onboard_pixel.begin();
@@ -224,8 +399,6 @@ void setup() {
 // loop()
 // -------------------------------------------------------
 void loop() {
-  int8_t ret;
-
   ret = mySerCmd.ReadSer();
   if (ret == 0) {
     mySerCmd.Print((char *) "ERROR: Urecognized command\r\n");
